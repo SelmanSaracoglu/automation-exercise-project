c05ec45abc71a9d9d4e01c155cd8824de41563b3
SelmanSaracoglu <selman.saracoglu@outlook.com>
2026-01-11 14:08:40 +0100

    refactor: upgrade project to Enterprise MDR architecture (v2)

--- cypress.config.js
+++ cypress.config.js
@@ -2,29 +2,23 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
 const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "**/*.feature",
     baseUrl: "https://automationexercise.com",
+    specPattern: "**/*.feature", 
+    video: false,
+    viewportWidth: 1280,
+    viewportHeight: 720,
     async setupNodeEvents(on, config) {
-      // 1. Initialize Cucumber Plugin
       await addCucumberPreprocessorPlugin(on, config);
-
-      // 2. Initialize Esbuild Preprocessor
       on(
         "file:preprocessor",
         createBundler({
           plugins: [createEsbuildPlugin(config)],
         })
       );
-
-      // 3. Return the modified config object
       return config;
     },
-    viewportWidth: 1920,
-    viewportHeight: 1080,
-    video: false,
   },
 });
